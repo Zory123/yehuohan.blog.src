1e4ee5ddd511ce245f9b0d91276fc9397a422af3
yehuohan <550034086@qq.com>
2018-03-22 15:32:04 +0800

    remove gitee.io

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 ## yehuohan.blog.src
 
  - [yehuohan.github.io](https://yehuohan.github.io)
-
- - [yehuohan.gitee.io](http://yehuohan.oschina.io)
--- hexo_deploy.cmd
+++ hexo_deploy.cmd
@@ -5,12 +5,6 @@ echo Press Ctrl + c to exit.
 pause
 
 echo Hexo : generate and deploy in gitee...
-copy /y _config_gitee.yml _config.yml
-hexo clean && ^
-hexo generate && ^
-hexo deploy && ^
-echo Hexo : generate and deploy in github... && ^
-copy /y _config_github.yml _config.yml && ^
 hexo clean && ^
 hexo generate && ^
 hexo deploy && ^
